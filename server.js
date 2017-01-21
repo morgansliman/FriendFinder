5d2c518cfc287ed5afba35277fab0593fce26639
morgansliman <morgansliman@gmail.com>
2017-01-20 20:31:45 -0500

    changing port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const survey = require('./app/routing/htmlRoutes');
 const api = require('./app/routing/apiRoutes');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(api);
 app.use(survey);
